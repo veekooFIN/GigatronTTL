f47581b4f869e09eab69b87fd519fcd17029d4d1
Vesa <90590852+veekooFIN@users.noreply.github.com>
2021-09-15 09:31:04 +0300

    Update gfxjulia.c

--- gfxjulia.c
+++ gfxjulia.c
@@ -82,7 +82,7 @@ void main(void) {
   for(y = 0; y < HEIGHT; y = y + YSTEP ) {
     for(x = 0; x < WIDTH; x = x + XSTEP ) {
       sx = (SCALE * (WIDTH/2.0 - x) / (WIDTH/2.0))*(-1);
-      sy = (SCALE * (HEIGHT/2.0 - y) / (HEIGHT/2.0))*(-1)/(WIDTH/HEIGHT);
+      sy = (SCALE * (HEIGHT/2.0 - y) / (HEIGHT/2.0))*(-0,5625);
       data = julia(sx, sy);
       drawPixel(x,y,col[data]);
     }
