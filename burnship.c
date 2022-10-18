a3ee0b9d53c09f2e8222903bf99d100d92777641
Vesa <90590852+veekooFIN@users.noreply.github.com>
2022-10-18 10:26:37 +0300

    Add files via upload

--- burnship.c
+++ burnship.c
@@ -27,7 +27,7 @@ int burnship(long real0, long imag0) {
 
   real = real0;
   imag = imag0;
-  for (i = 0; i < 63; i++)
+  for (i = 0; i < 15; i++)
   {
     realq = (real * real) >> NORM_BITS;
     imagq = (imag * imag) >> NORM_BITS;
@@ -46,18 +46,36 @@ void drawPixel(int x, int y, int color)
 }
 
 void main(void) {
-  int x, y, data, col;
+  int x, y, data;
+  int col[16];
   long realmin, imagmin, realmax, imagmax;
   long deltareal, deltaimag, real0, imag0;  
   
-  realmin = (long) ((-2.5) * (float) F);
-  realmax = (long) ((1.0) * (float) F);
-  imagmin = (long) ((-1.0) * (float) F);
-  imagmax = (long) ((1.0) * (float) F); 
+  col[14] = 0x01;
+  col[13] = 0x02;
+  col[12] = 0x03;
+  col[11] = 0x07;
+  col[10] = 0x0b;
+  col[9] = 0x0f;
+  col[8] = 0x0e;
+  col[7] = 0x0d;
+  col[6] = 0x0c;
+  col[5] = 0x3c;
+  col[4] = 0x38;
+  col[3] = 0x34;
+  col[2] = 0x30;
+  col[1] = 0x20;
+  col[0] = 0x10;
+  col[15] = 0x00;
+  
+  realmin = (long) ((-2.1) * (float) F);
+  realmax = (long) ((1.2) * (float) F);
+  imagmin = (long) ((-1.7) * (float) F);
+  imagmax = (long) ((0.7) * (float) F); 
    
   for(y = 0; y < HEIGHT; y++ ) {
     for(x = 0; x < WIDTH; x++ ) {
-      drawPixel(x,y,0);
+      drawPixel(x,y,col[15]);
     }
   } 
   
@@ -69,8 +87,7 @@ void main(void) {
     imag0 = imagmax;
     for(y = 0; y < HEIGHT; y++ ) {
       data = burnship(real0, imag0);
-      col = 63 - data;
-      drawPixel(x,y,col);
+      drawPixel(x,y,col[data]);
       imag0 -= deltaimag;
     }
     real0 += deltareal;
