2b34b4e3db6205898b029731d6d74e5bc6d4e925
Vesa <90590852+veekooFIN@users.noreply.github.com>
2024-02-04 17:25:13 +0200

    Add files via upload

---
+++ old/rndburnship.c
@@ -0,0 +1,117 @@
+/*   _____                                 _____                                                                                                                
+  __|   _ |__  ______  ______  ____     __| __  |__  ____  ____      ____  __   _  ____   _  ______  ____   _   ______  _____  _____   ____  ____   _  ______   
+ \  \  //    ||   ___||   ___||    \   |  |/ /     ||    ||    |    |    ||  | | ||    \ | ||   ___||    \ | | |   ___|/     \|     \ |    ||    \ | ||   ___|  
+ |\  \//     ||   ___| `-.`-. |     \  |     \     ||    ||    |_  _|    ||  |_| ||     \| ||   ___||     \| | |   |__ |     ||      \|    ||     \| ||   |  |  
+ |_\__/    __||______||______||__|\__\ |__|\__\  __||____||______||______||______||__/\____||______||__/\____| |______|\_____/|______/|____||__/\____||______|  
+    |_____|                               |_____|                                                                                                               
+                                           _                                                                                                                    
+  _____   __   _  ____  ______  __  __    / |  _____   ____  _____    __  __    _                                                                               
+ /     \ |  | | ||    ||   ___||  |/ /   / /  |     \ |    ||     | _|  |_\ \  //                                                                               
+ |     | |  |_| ||    ||   |__ |     \  |_/   |      \|    ||     \|_    _|\ \//                                                                                
+ \___/\_\|______||____||______||__|\__\       |______/|____||__|\__\ |__|  /__/                                                                                 
+                                                  _                                                                                                             
+   __    _ ______  ______  __  __  _____  _____   / |______  ____  ____   _                                                                                     
+  \  \  //|   ___||   ___||  |/ / /     \/     \ / /|   ___||    ||    \ | |                                                                                     
+   \  \// |   ___||   ___||     \ |     ||     ||_/ |   ___||    ||     \| |                                                                                     
+    \__/  |______||______||__|\__\\_____/\_____/    |___|   |____||__/\____|*/ 
+
+/*----------------------------------------------------------------------+
+ |                                                                      |
+ |             rndburnship.c -- demonstrate fractal in gfx              |
+ |                                                                      |
+ +----------------------------------------------------------------------*/
+ 
+// Standard includes
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <gigatron/sys.h>
+
+#define HEIGHT 120
+#define WIDTH 160
+#define SCALE 1.7
+#define YSTEP 1
+#define XSTEP 1
+
+static unsigned char sm[HEIGHT][WIDTH]; 
+
+int burnship(float x, float y) {
+ float zz;
+ float a;
+ float b;
+ float a2;
+ float b2;
+ float atemp;
+ int i;
+  
+  a = 0;
+  b = 0;  
+  i = 0;
+  while (i < 15)
+  {
+    a2 = a * a;
+    b2 = b * b;
+    zz = a2 + b2;
+    if(zz > 4) break;
+    
+    atemp = a2 - b2 + x;
+    b = fabs(2.0 * a * b) + y;
+    a = atemp;
+    i++;
+  }
+  return i;
+}
+
+void drawPixel(int x, int y, int color)
+{
+  screenMemory[y][x] = color;
+}
+
+void main(void) {
+  int x, y, data, n;
+  int col[16];
+  float sx, sy;  
+
+  srand(time(0));
+  
+  SYS_SetMode(3);
+  
+  col[14] = 0x01;
+  col[13] = 0x02;
+  col[12] = 0x03;
+  col[11] = 0x07;
+  col[10] = 0x0b;
+  col[9] = 0x0f;
+  col[8] = 0x0e;
+  col[7] = 0x0d;
+  col[6] = 0x0c;
+  col[5] = 0x3c;
+  col[4] = 0x38;
+  col[3] = 0x34;
+  col[2] = 0x30;
+  col[1] = 0x20;
+  col[0] = 0x10;
+  col[15] = 0x00;
+   
+  for(y = 0; y < HEIGHT; y = y + YSTEP ) {
+    for(x = 0; x < WIDTH; x = x + XSTEP ) {
+      drawPixel(x,y,col[15]);
+    }
+  } 
+    
+  for(n = 0; n < 19200; n = n + 1 ) {
+    y = rand() % HEIGHT;
+    x = rand() % WIDTH;
+    
+    if(sm[y][x] == 'y') n--;
+    
+    if(sm[y][x] != 'y') {    
+      sx = -0.4 + (SCALE * (WIDTH/2.0 - x) / (WIDTH/2.0))*(-1);
+      sy = -0.5 + (SCALE * (HEIGHT/2.0 - y) / (HEIGHT/2.0))*(-0.75);
+      data = burnship(sx, sy);
+      drawPixel(x,y,col[data]);
+      sm[y][x] = 'y';
+    }
+  }
+  SYS_SetMode(0);
+}
